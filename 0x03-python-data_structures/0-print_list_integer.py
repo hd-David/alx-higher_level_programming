686c60052d7e2fc9cb6111941e12cbd28fac79cb
hd-David <mwewahoby@gmail.com>
2024-01-08 22:51:07 +0200

    First commit

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,12 @@
+def print_list_integer(my_list=[]):
+    """
+    Print all integers of a list.
+
+    Args:
+    - my_list (list): The input list containing integers.
+
+    Returns:
+    - None
+    """
+    for num in my_list:
+        print("{}".format(num))
---
+++ 0x03-python-data_structures/README.md
@@ -0,0 +1,3 @@
+# 0x03. Python - Data Structures: Lists, Tuples
+
+This repository contains Python scripts that cover the topic of Data Structures, specifically focusing on Lists and Tuples.
