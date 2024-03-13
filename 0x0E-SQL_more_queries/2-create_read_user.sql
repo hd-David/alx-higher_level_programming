460d653be904d43d50e661217fb30e55dca65932
hd-David <mwewahoby@gmail.com>
2024-03-13 15:11:28 -0700

    Read user

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -5,7 +5,7 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'%' IDENTIFIED BY 'user_0d_2_pwd';
 
 -- Grant SELECT privilege to user_0d_2 on the hbtn_0d_2 database
-GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'%';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'%' IDENTIFIED BY 'user_0d_2_pwd';
 
 -- Flush privileges to apply the changes
 FLUSH PRIVILEGES;
