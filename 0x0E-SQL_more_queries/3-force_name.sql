42880dd5cea6b86041e59d1919126efa180dd0b7
hd-David <mwewahoby@gmail.com>
2024-03-14 23:42:23 +0200

    Creating a table

---
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -0,0 +1,5 @@
+-- Create the force_name table if it doesn't already exist
+CREATE TABLE IF NOT EXISTS force_name (
+    id INT AUTO_INCREMENT PRIMARY KEY, -- An auto-incremented integer for the primary key
+    name VARCHAR(256) NOT NULL -- A non-null VARCHAR(256) column for names
+)
