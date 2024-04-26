a2107d2abcb52a898618a92e76faba21acfc85cc
hd-David <mwewahoby@gmail.com>
2024-04-26 14:32:41 -0700

    Fetch url content

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+import urllib.request
+
+def fetch_status(url):
+    """
+    Fetches the content of the given URL and displays information about the response body.
+
+    Args:
+    - url (str): The URL to fetch.
+
+    Returns:
+    - str: The content of the response body.
+    """
+
+    # Open the URL using urllib.request.urlopen() and use a 'with' statement to ensure\
+    # proper closing of the connection
+
+    with urllib.request.urlopen(url) as response:
+        body = response.read().decode('utf-8')
+
+    print('Body response')
+    print('\t- type: {}'.format(type(body)))
+    print('\t- content: {}'.format(body))
+
+    return body
+
+
+#URL to fetch
+url = 'https://alx-intranet.hbtn.io/status'
+
+# Fetch the status and store the response body in a variable
+response_body = fetch_status(url)
+        
