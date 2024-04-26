e3034906d7c70cf891b4b8274ec7e0a14a9d5ace
hd-David <mwewahoby@gmail.com>
2024-04-26 14:57:07 -0700

    Fixing output

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,9 +2,11 @@
 
 import urllib.request
 
+
 def fetch_status(url):
     """
-    Fetches the content of the given URL and displays information about the response body.
+    Fetches the content of the given URL and displays 
+    information about the response body.
 
     Args:
     - url (str): The URL to fetch.
@@ -13,7 +15,8 @@ def fetch_status(url):
     - str: The content of the response body.
     """
 
-    # Open the URL using urllib.request.urlopen() and use a 'with' statement to ensure\
+    # Open the URL using urllib.request.urlopen() and use a 'with'
+    # statement to ensure\
     # proper closing of the connection
 
     with urllib.request.urlopen(url) as response:
@@ -24,14 +27,13 @@ def fetch_status(url):
     print('\t- type: {}$'.format(type(body)))
     print('\t- content: {}$'.format(body))
     print("\t- utf8 content: {}$".format(utf8_content))
-    
+
     
     return body
 
 
 #URL to fetch
-url = 'https://alx-intranet.hbtn.io/status'
+url = urllib.request.Request( 'https://alx-intranet.hbtn.io/status')
 
 # Fetch the status and store the response body in a variable
 response_body = fetch_status(url)
-        
