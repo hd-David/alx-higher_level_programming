f78f4b596b26e99366e2ab630a9e327e18618663
hd-David <mwewahoby@gmail.com>
2024-04-26 15:24:02 -0700

    Response header

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+import urllib.request
+import sys
+
+
+"""Displays the X-Request-Id header variable of a request to a given URL.
+
+Usage: ./1-hbtn_header.py <URL>
+"""
+
+url = sys.argv[1]
+
+try:
+    with urllib.request.urlopen(url) as response:
+        request_id =response.headers.get('X-Request-Id')
+        if request_id:
+            print(request_id)
+        else:
+            print('X-Request-Id not found in the response headers.')
+except urllib.error.URLError as e:
+                  print("Error fetching URL:", e.reason)
+                  sys.exit(1)
